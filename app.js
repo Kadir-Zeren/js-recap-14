f267776862c05b9ed874f4f33df36a2ec2ca3e1d
kadir-zeren <k4dzeren@gmail.com>
2023-11-30 08:20:40 +0300

    v3

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const selectionArticle = document.querySelector(".selection");
 const yourChoiceDiv = document.getElementById("your-choice");
 
 let userSelectImg = document.createElement("img");
+let pcSelectImg = document.createElement("img");
 
 selectionArticle.addEventListener("click", (e) => {
   console.log(e.target.id);
@@ -10,5 +11,13 @@ selectionArticle.addEventListener("click", (e) => {
     userSelectImg.src = `./assets/${e.target.id}.png`;
     userSelectImg.alt = e.target.id;
     yourChoiceDiv.appendChild(userSelectImg);
+    createPcSelection();
   }
 });
+
+const createPcSelection = () => {
+  const pcArr = ["rock", "paper", "scissor"];
+  const pcRandom = pcArr[Math.floor(Math.random() * 3)];
+  pcSelectImg.src = `./assets/${pcRandom}.png`;
+  pcSelect.alt = pcRandom;
+};
